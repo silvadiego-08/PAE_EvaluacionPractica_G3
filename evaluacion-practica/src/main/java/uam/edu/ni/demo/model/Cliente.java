b330dc8b175e5818595a6453d460a669d3ce8615
diegoSilva <djsilvacubas@uamv.edu.ni>
2026-09-21 16:38:33 -0600

    feature: cliente model

---
+++ evaluacion-practica/src/main/java/uam/edu/ni/demo/model/Cliente.java
@@ -0,0 +1,12 @@
+package uam.edu.ni.demo.model;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Cliente {
+    private String nombre_completo;
+    private LocalDateTime fecha_servicio;
+    private LocalTime hora_servicio;
+    private String Servicio;
+    private boolean estado;
+}
